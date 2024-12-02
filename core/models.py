b84ecb68b7080488f323ac97fa05c4900509951b
MikeHuggar <rogeriofmateus04@gmail.com>
2024-12-02 17:55:29 -0300

    Speakers

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Speaker(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField('slug')
+    photo = models.URLField('photo')
+    website = models.URLField('website', blank=True)
+    description = models.TextField('description', blank=True)
+
+    class Meta:
+        verbose_name = 'palestrante'
+        verbose_name_plural = 'palestrantes'
+    
+    def __str__(self):
+        return self.name
