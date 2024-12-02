4ee9f618ce959d0ddcffbc16c906c19f6c934781
MikeHuggar <rogeriofmateus04@gmail.com>
2024-12-02 15:19:06 -0300

    keynote speakers

--- core/tests/test_view_home.py
+++ core/tests/test_view_home.py
@@ -12,3 +12,16 @@ class HomeTest(TestCase):
         self.assertTemplateUsed(self.response, 'index.html')
     def test_link_subscription(self):
         self.assertContains(self.response, 'href="{}"'.format(r('subscriptions:new')))
+    def test_speakers(self):
+        contents = [
+            'Grace Hopper',
+            'https://abre.ai/hopper-pic',
+            'Alan Turing',
+            'https://abre.ai/hopper-pic'
+        ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+    def test_speaker_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
